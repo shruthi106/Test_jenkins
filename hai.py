3fac4d343c23efde84a8d87a15f93fc9a0eb1834
shruthi0911 <saki.shruthi@gmail.com>
2016-06-08 12:52:00 +0530

    commit

---
+++ hai.py
@@ -0,0 +1,26 @@
+name="madhu"
+print "hai", name
+
+A="This is Python"
+print A[8]
+print A[2]
+print A[11]
+print A[10:-2]
+print A.find("t")
+
+A=["Madhu"]
+A.append("kiran")
+print A
+
+A=["Madhu"]
+A.extend("kiran")
+print A
+
+
+ele = {"H":"Hydrogen","C":"Carbon","N":"Nitrogen","O":"Oxygen"}
+print ele.keys()
+print ele.values()
+print ele.items()
+ele.update({"C":"Calcium"})
+print ele.items()
+
